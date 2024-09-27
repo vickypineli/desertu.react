59b3e95025b32cde427d201b796930c872594e3d
vickypineli <v.pinero70clase@gmail.com>
2024-09-27 11:50:35 +0200

    l corrected the image carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -12,31 +12,18 @@ const Carousel = () => {
   };
 
   return (
-    <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExample" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src="/assets/desertu berria0.jpg" className="d-block w-100" alt="Imagen 1" />
+          <img src="/assets/plaza-pormetxeta1.jpg" className="d-block w-100" alt="Plaza PORmetxeta" />
         </div>
         <div className="carousel-item">
-          <img src="/assets/plaza-desierto2.jpg" className="d-block w-100" alt="Imagen 2" />
+          <img src="/assets/plaza-desierto2.jpg" className="d-block w-100" alt="Plaza Desierto" />
         </div>
         <div className="carousel-item">
-          <img src="/assets/plaza-desierto1.png" className="d-block w-100" alt="Imagen 3" />
-        </div>
-        <div className="carousel-item">
-          <img src="/assets/plaza-pormetxeta.jpg" className="d-block w-100" alt="Imagen 4" />
+          <img src="/assets/desertu berria0.jpg" className="d-block w-100" alt="Barrio Desierto" />
         </div>
       </div>
-
-      {/* Botones de control del carrusel */}
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
 
       {/* Textos superpuestos */}
       <div className="carousel-text" onMouseOver={handleOver} onMouseOut={handleOut}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Importa Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // Importa Bootstrap JS (necesario para que funcione el carrusel)
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
