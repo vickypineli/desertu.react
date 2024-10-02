27b0b5a1ea2f6c10fb4d8451bb4ae08ef21189a6
vickypineli <v.pinero70clase@gmail.com>
2024-10-02 21:37:42 +0200

    add animation in scrolltopbutton

--- src/components/scrollToTopButton/ScrollToTopButton.jsx
+++ src/components/scrollToTopButton/ScrollToTopButton.jsx
@@ -1,47 +1,43 @@
-// src/components/scrollToTopButton/ScrollToTopButton.jsx
-
+// src/components/ScrollToTopButton.jsx
 import { useState, useEffect } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
-
 import './ScrollToTopButton.css';
 
 const ScrollToTopButton = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    
-    // Hook para manejar la visibilidad del botón basado en el scroll
-    useEffect(() => {
-        const toggleVisibility = () => {
-        if (window.scrollY > 400) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-        };
-        // Añadir el event listener para escuchar el evento de scroll
-        window.addEventListener('scroll', toggleVisibility);
-        // Limpiar el event listener al desmontar el componente
-            return () => window.removeEventListener('scroll', toggleVisibility);
-        }, []);
+  const [isVisible, setIsVisible] = useState(false);
 
-        // Función para desplazar hacia la parte superior de la página
-    const scrollToTop = () => {
-        console.log("Scroll hacia arriba"); // Log para confirmar la ejecución
-        window.scrollTo({
-        top: 0,
-        behavior: 'smooth', // Desplazamiento suave
-        });
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 400) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
     };
 
-  // Renderizar el componente
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    console.log("Scroll hacia arriba");
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div>
-      {isVisible && (
-        <button onClick={scrollToTop} className="scroll-to-top-button">
-          <FaArrowUp />
-        </button>
-      )}
+      <button
+        onClick={scrollToTop}
+        className={`scroll-to-top-button ${isVisible ? 'slide-in' : 'slide-out'}`}
+      >
+        <FaArrowUp />
+      </button>
     </div>
   );
+};
 
-}
 export default ScrollToTopButton;
